54b4f2f168ff4ca64472c9a7bac56007c3dd3e78
André Eduardo Menegasso <meneghasso@gmail.com>
2022-05-16 17:35:30 -0300

    Update ApplicationTests.java

--- src/test/java/br/com/TestR/ApplicationTests.java
+++ src/test/java/br/com/TestR/ApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.alura.alura;
+package br.com.TestR;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AluraApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
